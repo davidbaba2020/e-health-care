75c53645b6b657bed4dc2cf871fe1b4d8b1be200
David Baba <davidbabasalifu2020@gmail.com>
2022-01-14 02:29:53 +0100

    Herouku set up

--- src/main/java/com/hda/dev/HealthDiagnosisApp.java
+++ src/main/java/com/hda/dev/HealthDiagnosisApp.java
@@ -2,7 +2,11 @@ package com.hda.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@Component
 @SpringBootApplication
 public class HealthDiagnosisApp {
 
